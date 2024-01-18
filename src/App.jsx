3d93ea1fbed334af80ae552c4603c27fa0c13c39
miasif <minhazul.islam.asif@gmail.com>
2024-01-18 17:02:01 +0600

    TaskBoard component create

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import TaskBoard from "./components/task/TaskBoard";
 
 export default function App() {
   return (
     <>
       <Header />
-      <HeroSection />
+      <div className=" flex flex-col justify-center items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+
       <Footer />
     </>
   );
---
+++ src/components/task/TaskBoard.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import SearchTask from "./SearchTask";
+import TaskAction from "./TaskAction";
+import TaskList from "./TaskList";
+
+export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      " I want to Learn React such than I can treat it like my slav.",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTask]);
+
+  return (
+    <section className="mb-20" id="tasks">
+      <div className="container">
+        <div className="p-2 flex justify-end">
+          <SearchTask />
+        </div>
+
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+          <TaskAction />
+          <TaskList tasks={tasks} />
+        </div>
+      </div>
+    </section>
+  );
+}
